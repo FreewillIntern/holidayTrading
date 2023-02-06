1ef912329d47bdcac1b648b6405b700f6cb79101
P2OM <heammapoom@gmail.com>
2023-02-06 10:58:13 +0700

    fig overhead

--- pages/index.vue
+++ pages/index.vue
@@ -2,9 +2,9 @@
     <NuxtLayout :name="window.width > 900 ? 'custom' : 'mobile'">
         <template #topLeft>
             <!-- TOP LEFT -->
-            <component :is="window.width > 550? resovePc : resoveMb" />
-            <!-- <SearchResponPc />
-            <SearchResponMb /> -->
+            <!-- <component :is="window.width > 550? resovePc : resoveMb" /> -->
+            <SearchResponPc v-if="window.width > 550" />
+            <SearchResponMb v-else />
         </template>
         <template #topRight>
             <!-- TOP RIGHT -->
@@ -28,10 +28,7 @@ export default {
     data(){
         return {
             window: useWindowSize(),
-            resovePc: resolveComponent("SearchResponPc"),
-            resoveMb: resolveComponent("SearchResponMb"),
         }
-       
     }
 }
 </script>
