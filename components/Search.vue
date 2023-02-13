ef806d431596f0f9185e251abdd95e953040c40d
P2OM <heammapoom@gmail.com>
2023-02-13 21:36:25 +0700

    add inkline and change select to inkline

--- components/Search.vue
+++ components/Search.vue
@@ -4,34 +4,26 @@
     <div class="w-full flex">
       <!-- Pc -->
       <div class="h-full w-full flex justify-evenly" v-show="window.width >= 550">
-        <div class="flex">
+        <div class="flex items-center">
           <h1 class="min-w-fit">Year :</h1>
-          <input list="years" v-model="year" v-maska data-maska="####"
-            class="inputYear ml-2 w-[180px] border-b-2 border-[rgb(84,84,84)] bg-transparent outline-none" />
-          <datalist id="years">
-            <option v-for="(y, i) in years" :key="i" :value="y"></option>
-          </datalist>
+          <i-select v-model="year" :options="filterYears" autocomplete placeholder="Select Year" @search="onSearchYear"
+            @input="onInputYear" />
         </div>
 
-        <div class="flex">
+        <div class="flex items-center">
           <h1 class="min-w-fit">Market :</h1>
-          <input list="markets" v-model="market" v-maska data-maska="HHHHHHHHHHHHHHHHHHHH"
-            data-maska-tokens="H:[a-zA-Z0-9]"
-            class="inputMarket ml-2 mr-2 w-[240px] border-b-2 border-[rgb(84,84,84)] bg-transparent outline-none uppercase" />
-          <datalist id="markets">
-            <option v-for="(m, i) in markets" :key="i" :value="m.mktcode"></option>
-          </datalist>
+          <i-select class="mr-2" v-model="market" :options="filterMarketCodes" autocomplete placeholder="Code" @search="onSearchMarket" @input="onInputMarket"/>
           <h1>:</h1>
-          <h1 class="ml-2 w-[200px] min-w-fit border-b-2 border-[rgb(84,84,84)]" v-if="window.width > 1100">
+          <h1 class="ml-2 w-[200px] h-[26px] min-w-fit border-b-2 border-[rgb(84,84,84)]" v-if="window.width > 1200">
             {{ getMktFullName.mktname }}
           </h1>
         </div>
 
-        <div class="flex">
-          <button @click="search"
-            class="px-[15px] py-[2px] bg-[rgba(255,255,255,0.3)] shadow-[0_0_10px_rgba(0,0,0,0.3)] hover:shadow-[0_0_15px_rgba(0,0,0,0.3)] rounded-3xl">
+        <div class="flex items-center">
+          <h1 @click="search"
+            class="px-[15px] py-[4px] bg-[rgba(255,255,255,0.3)] hover:bg-[rgba(255,255,255,0.5)] shadow-[0_0_10px_rgba(0,0,0,0.3)] hover:shadow-[0_0_15px_rgba(0,0,0,0.5)] cursor-pointer rounded-3xl">
             Search
-          </button>
+          </h1>
         </div>
       </div>
       <!-- Pc -->
@@ -74,6 +66,9 @@
 import { vMaska } from "maska";
 import { useMainStore } from "~~/stores/data";
 import { useWindowSize } from "@vueuse/core";
+import { ComboBox } from "@progress/kendo-vue-dropdowns";
+import { filterBy } from "@progress/kendo-data-query";
+import { fontSizeIcon, paddingTopIcon } from "@progress/kendo-svg-icons";
 
 export default {
   setup() {
@@ -81,26 +76,38 @@ export default {
     return { store };
   },
   directives: { maska: vMaska },
+  components: {
+    combobox: ComboBox,
+  },
   data() {
     return {
+      url: useRuntimeConfig().public.apiBase,
       window: useWindowSize(),
       years: [],
       markets: [],
-      year: "",
-      market: "",
+      marketCodes: [],
+      year: null,
+      market: null,
+      filterYears: [],
+      filterMarketCodes: [],
     };
   },
   mounted() {
-    fetch(`https://10.22.26.103/beam/market`)
+    fetch(`${this.url}market`)
       .then((response) => response.json())
       .then((result) => {
         this.markets = result.data;
+        for (let i = 0; i < result.data.length; i++) {
+          this.marketCodes.push({id: i+1, label: result.data[i].mktcode})
+          this.filterMarketCodes.push({id: i+1, label: result.data[i].mktcode})
+        };
         this.store.marketCode = result.data[0].mktcode;
         this.store.marketName = result.data[0].mktname;
         this.store.markets = result.data;
         fetch(
-          `https://10.22.26.103/beam/holiday?mkt=${this.markets[0].mktcode
-          }&year=${new Date().getFullYear().toString()}`
+          `${this.url}holiday?mkt=${this.markets[0].mktcode}&year=${new Date()
+            .getFullYear()
+            .toString()}`
         )
           .then((response) => response.json())
           .then((result) => (this.store.holidays = result.data));
@@ -108,7 +115,8 @@ export default {
 
     const setYear = new Date().getFullYear();
     for (let i = 1; i < 14; i++) {
-      this.years.push((setYear - (11 - i)).toString());
+      this.years.push({ id: i, label: (setYear - (11 - i)).toString() });
+      this.filterYears.push({ id: i, label: (setYear - (11 - i)).toString() });
     }
 
     this.store.year = new Date().getFullYear().toString();
@@ -116,55 +124,72 @@ export default {
   methods: {
     search() {
       if (
-        this.year != "" &&
-        this.market != "" &&
-        this.markets.find((mkt) => mkt.mktcode === this.market) != undefined
+        this.year != null &&
+        this.market != null
       ) {
-        fetch(
-          `https://10.22.26.103/beam/holiday?mkt=${this.market}&year=${this.year}`
-        )
+        fetch(`${this.url}holiday?mkt=${this.market.label}&year=${this.year.label}`)
           .then((response) => response.json())
           .then((result) => (this.store.holidays = result.data));
 
-        this.store.year = this.year;
-        this.store.marketCode = this.markets.find((mkt) => mkt.mktcode === this.market).mktcode;
-        this.store.marketName = this.markets.find((mkt) => mkt.mktcode === this.market).mktname;
+        this.store.year = this.year.label;
+        this.store.marketCode = this.market.label
+        this.store.marketName = this.markets.find(
+          (mkt) => mkt.mktcode === this.market.label
+        ).mktname;
 
         gtag("event", "search", {
-          search_term: "Year: " + this.year + ", Market Code: " + this.market,
+          search_term: "Year: " + this.year.label + ", Market Code: " + this.market.label,
         });
-
-        this.year = "";
-        this.market = "";
-      } else if (this.year == "" && this.market == "") {
-        alert("Please enter the year and market.");
-      } else if (this.year == "") {
-        alert("Please enter the year.");
-      } else if (this.market == "") {
-        alert("Please enter the market.");
       }
-      // else if (this.years.includes(this.year) === false) {
-      //   alert("Sorry, the year you selected does not exist.");
-      // }
-      else if (
-        this.markets.find((mkt) => mkt.mktcode === this.market) === undefined
-      ) {
-        alert("Sorry, the market you selected does not exist.");
+    },
+    onInputYear(event) {
+      const value = event.target.value;
+      event.target.value = value.replace(/[^0-9]/g, "");
+      if (value.length >= 4) {
+        event.target.value = value.substr(0, 4);
       }
+    },
+    onSearchYear(query) {
+      if (query != null) {
+        query = query.substr(0, 4);
+      }
+      this.filterYears = this.years
+        .filter((option) => {
+          return option.label.toLowerCase().includes((query || '').toLowerCase());
+        });
+    },
+    onInputMarket(event) {
+      const value = event.target.value;
+      event.target.value = value.toUpperCase();
+      if (value.length >= 20) {
+        event.target.value = value.substr(0, 20);
+      }
+    },
+    onSearchMarket(query) {
+      this.filterMarketCodes = this.marketCodes
+        .filter((option) => {
+          return option.label.toLowerCase().includes((query || '').toLowerCase());
+        });
     },
   },
   computed: {
     getMktFullName() {
-      return this.markets.find((mkt) => mkt.mktcode === this.market) || "";
+      return this.markets.find((mkt) => mkt.mktcode === this.market.label) || "";
     },
   },
 };
 </script>
 
 <style>
+.input {
+  width: 180px;
+  height: 30px;
+  margin-left: 8px;
+}
+
 @media only screen and (max-width: 1400px) {
-  .inputYear {
-    width: 90px;
+  .input {
+    width: 120px;
   }
 
   .inputMarket {
