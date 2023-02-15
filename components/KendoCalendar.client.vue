5da01f8a4cfa4ef40462edd6e235673c0115fbcf
noppornSak <nopporn.sakulrat@gmail.com>
2023-02-15 14:33:23 +0700

    Edit holiday description

--- components/KendoCalendar.client.vue
+++ components/KendoCalendar.client.vue
@@ -1,53 +1,57 @@
 <template>
-  <!-- <div class="holiday-description grid grid-cols-3 mb-10 bg-[rgba(32,32,32,0.95)] shadow-[_3px_3px_15px_rgba(0,0,0,0.8)] rounded-xl items-center text-white">
-    <div class="flex" id="holiday-N">
-      <p>N</p>
-      <p>= No Trading and Settlement</p>
+  <div class="grid justify-items-center overflow-auto items-center h-full">
+    <div
+      class="holiday-description grid grid-cols-3 mb-10 bg-[rgba(32,32,32,0.95)] shadow-[_3px_3px_15px_rgba(0,0,0,0.8)] rounded-xl items-center text-white"
+    >
+      <div class="flex" id="holiday-N">
+        <p class="cantrade-code ct-n">N</p>
+        <p>= No Trading and Settlement</p>
+      </div>
+      <div class="flex" id="holiday-t">
+        <p class="cantrade-code ct-t">T</p>
+        <p>= Trade only ( No settlement )</p>
+      </div>
+      <div class="flex" id="holiday-S">
+        <p class="cantrade-code ct-s">S</p>
+        <p>= Settlement only ( No Trading )</p>
+      </div>
     </div>
-    <div class="flex" id="holiday-t">
-      <p>T</p>
-      <p>= Trade only ( No settlement )</p>
-    </div>
-    <div class="flex" id="holiday-S">
-      <p>S</p>
-      <p>= Settlement only ( No Trading )</p>
-    </div>
-  </div> -->
-  <div
-    class="grid justify-items-center overflow-auto overflow-y-auto items-center h-full "
-    :class="{
-      'grid-cols-4': columns >= 4,
-      'grid-cols-3': columns == 3,
-      'grid-cols-2': columns == 2,
-      'grid-cols-1': columns == 1,
-    }"
-  >
-    <!-- Calendar 12 month -->
-    <calendar
-      v-for="month in months"
-      :monthlyLeave="monthlyLeave[month]"
-      :month="month"
-      :year="year"
-      :key="month"
-      @click-left="clickShowCell"
-      @click-right="clickEventCell"
-    ></calendar>
+    <div
+      class="grid gap-4"
+      :class="{
+        'grid-cols-4': columns >= 4,
+        'grid-cols-3': columns == 3,
+        'grid-cols-2': columns == 2,
+        'grid-cols-1': columns == 1,
+      }"
+    >
+      <!-- Calendar 12 month -->
+      <calendar
+        v-for="month in months"
+        :monthlyLeave="monthlyLeave[month]"
+        :month="month"
+        :year="year"
+        :key="month"
+        @click-left="clickShowCell"
+        @click-right="clickEventCell"
+      ></calendar>
 
-    <!-- Dialog Information -->
-    <DialogDateInformation
-      v-if="informationDialogVisible"
-      :dialogVisible="informationDialogVisible"
-      :dataDateSelected="dataDateSelected"
-      @state-information-dialog="updateInformationDialogState"
-    />
+      <!-- Dialog Information -->
+      <DialogDateInformation
+        v-if="informationDialogVisible"
+        :dialogVisible="informationDialogVisible"
+        :dataDateSelected="dataDateSelected"
+        @state-information-dialog="updateInformationDialogState"
+      />
 
-    <!-- Dialog Event -->
-    <EventDialog
-      v-if="eventDialogVisible"
-      :dialogVisible="eventDialogVisible"
-      :dataDateSelected="dataDateSelected"
-      @state-event-dialog="updateEventDialogState"
-    />
+      <!-- Dialog Event -->
+      <EventDialog
+        v-if="eventDialogVisible"
+        :dialogVisible="eventDialogVisible"
+        :dataDateSelected="dataDateSelected"
+        @state-event-dialog="updateEventDialogState"
+      />
+    </div>
   </div>
 </template>
 
@@ -151,6 +155,15 @@ export default {
 <style scoped>
 .holiday-description {
   padding: 1rem;
+}
 
+.cantrade-code {
+  outline: solid red;
+  text-align: center;
+  width: 2rem;
+  height: 2rem;
+  padding: 1rem;
+  margin-right: 1rem;
+  
 }
 </style>
