13205694554e0b15dbccb3325dc53a864df79dde
P2OM <heammapoom@gmail.com>
2023-02-20 18:34:35 +0700

    edit loading

--- components/SmartClock.vue
+++ components/SmartClock.vue
@@ -1,12 +1,12 @@
 <template>
-    <div
-        class="w-full h-full bg-[rgba(32,32,32,0.95)] shadow-[_3px_3px_15px_rgba(0,0,0,0.8)] rounded-xl flex flex-col items-center text-white">
-        <div v-show="!loading" class="flex flex-col justify-center items-center h-[100%]">
-            <p>Loading ...</p>
-        </div>
-        <div v-show="loading" class="flex flex-col justify-center items-center h-[100%]">
-            <p>{{ timeThree }} | {{ timeOne }}</p>
-            <!-- <p> {{ store.marketCode }} | {{ store.marketName }} </p> -->
+    <div class="w-full h-full">
+        <div v-show="!loading" class="hidden"></div>
+        <div
+            v-show="loading" class="w-full h-full bg-[rgba(32,32,32,0.95)] shadow-[_3px_3px_15px_rgba(0,0,0,0.8)] rounded-xl flex flex-col items-center text-white">
+            <div class="flex flex-col justify-center items-center h-[100%]">
+                <p>{{ timeThree }} | {{ timeOne }}</p>
+                <!-- <p> {{ store.marketCode }} | {{ store.marketName }} </p> -->
+            </div>
         </div>
     </div>
 </template> 
