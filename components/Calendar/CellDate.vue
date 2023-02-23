4c354f1c92dd98d38c353140c5e2c7e498826d94
noppornSak <nopporn.sakulrat@gmail.com>
2023-02-23 16:46:54 +0700

    Convert javascript to typescript on CellDate component

--- components/Calendar/CellDate.vue
+++ components/Calendar/CellDate.vue
@@ -11,8 +11,26 @@
   </td>
 </template>
 
-<script>
-export default {
+<script lang="ts">
+import { defineComponent } from "vue";
+
+type StringNull = string | undefined | null;
+
+interface DataShowDate {
+  date: Date | undefined;
+  isWeekend: boolean;
+  isHoliday: boolean;
+  cantrade?: StringNull;
+  description?: StringNull;
+}
+interface DataEventDate {
+  date: Date | undefined;
+  isHoliday: boolean;
+  cantrade?: StringNull;
+  description?: StringNull;
+}
+
+export default defineComponent({
   props: {
     formattedValue: {
       type: String,
@@ -45,7 +63,7 @@ export default {
   emits: ["click-left-cell", "click-right-cell"],
 
   computed: {
-    styleCss() {
+    styleCss(): any {
       if (this.isHoliday) {
         return this.cellStyleHolidays;
       } else if (this.isWeekend) {
@@ -54,36 +72,37 @@ export default {
         return this.cellStyle;
       }
     },
-    cellStyle() {
+    cellStyle(): object {
       return {
         backgroundColor: "rgba(228,228,228,0.3)",
         color: "black",
       };
     },
-    cellStyleWeekEnd() {
+    cellStyleWeekEnd(): object {
       return {
         backgroundColor: "rgba(215,215,215,1)",
         color: "black",
       };
     },
-    cellStyleHolidays() {
+    cellStyleHolidays(): object {
       const style = {
         color: "black",
+        backgroundColor: "",
       };
       if (this.cantrade === "N") {
         style.backgroundColor = "rgba(255,153,153)";
       } else if (this.cantrade === "T") {
         style.backgroundColor = "rgba(255,255,153)";
       } else if (this.cantrade === "S") {
-        style.backgroundColor = "rgba(153,204,255)"; // blue "rgba(153,204,255)"
+        style.backgroundColor = "rgba(153,204,255)";
       }
       return style;
     },
   },
 
   methods: {
-    handleLeftClick() {
-      const data = {
+    handleLeftClick(): void {
+      const data: DataShowDate = {
         date: this.value,
         isWeekend: this.isWeekend,
         isHoliday: this.isHoliday,
@@ -94,9 +113,9 @@ export default {
       }
       this.$emit("click-left-cell", data);
     },
-    handleRightClick(event) {
+    handleRightClick(event: Event): void {
       event.preventDefault();
-      const data = {
+      const data: DataEventDate = {
         date: this.value,
         isHoliday: this.isHoliday,
       };
@@ -107,5 +126,5 @@ export default {
       this.$emit("click-right-cell", data);
     },
   },
-};
+});
 </script>
